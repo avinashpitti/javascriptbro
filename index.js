8a290babfd620bbb6b6292eff386c5e1d3ba45ba
avinashpitti <avinash87129@gmail.com>
2025-08-15 11:47:57 +0530

    added few arithmetic operators

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-document.getElementById("myH1").textContent="Hello Everyone"
-document.getElementById("myP").textContent="I like Pizza."
+let students=300;
+students=students+3;
+console.log(students);
+
+let teachers=9;
+teachers=teachers-2;
+document.writeln("The number of teachers is ", teachers,"<br>"); 
+
+let staff=11;
+staff+=4;
+document.writeln("The total number of employees is ", staff,"<br>");
+
+let security=3;
+document.getElementById("security").textContent="The no. of security personnel are: "+security;
+
+let cleaning=4;
+document.getElementById("cleaning").textContent="The no. of cleaning professionals are: "+cleaning;
