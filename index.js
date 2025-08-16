0721b79501781b19df3e0370002a7a875b9f48fa
avinashpitti <avinash87129@gmail.com>
2025-08-16 18:10:55 +0530

    added onclick event

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-let students=300;
-students=students+3;
-console.log(students);
+let username;
 
-let teachers=9;
-teachers=teachers-2;
-document.writeln("The number of teachers is ", teachers,"<br>"); 
-
-let staff=11;
-staff+=4;
-document.writeln("The total number of employees is ", staff,"<br>");
-
-let security=3;
-document.getElementById("security").textContent="The no. of security personnel are: "+security;
-
-let cleaning=4;
-document.getElementById("cleaning").textContent="The no. of cleaning professionals are: "+cleaning;
+document.getElementById("btn").onclick=function(){
+username=document.getElementById("user").value;
+document.getElementById("h").textContent=`Hello ${username}`;
+}
