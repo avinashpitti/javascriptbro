65e177cc5f117ec905c9f73d215928a6137afc8e
avinashpitti <avinash87129@gmail.com>
2025-08-16 21:41:17 +0530

    added input window prompt

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let username;
+// change the datatype of a value to another(strings,numbers,booleans)
+let age=window.prompt("How old are you?");
+age+=1;
+// here it considers as a string.
+console.log(age,typeof age);
 
-document.getElementById("btn").onclick=function(){
-username=document.getElementById("user").value;
-document.getElementById("h").textContent=`Hello ${username}`;
-}
+let years=window.prompt("how many years do you have?");
+years=Number(years);
+years+=1;
+// here it considers as a number.
+console.log(years,typeof years);
+
