164090ca22780c844365cf0c411348994a6a950e
avinashpitti <avinash87129@gmail.com>
2025-08-17 11:45:55 +0530

    added const variable in the webpage

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // const
 
-const pi=3.14
+const PI=3.14
 let radius;
 let circumference;
 
-radius=window.prompt("Enter the radius of a circle");
-radius=Number(radius);
 
-circumference=2*pi*radius;
+document.getElementById("btn").onclick=function(){
+radius=document.getElementById("inp").value ;
+radius=Number(radius);
+circumference=2*PI*radius;
+document.getElementById("hh").textContent=circumference;
+}
+
 
 console.log(circumference);
