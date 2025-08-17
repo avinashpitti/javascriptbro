552614277cb9877e50d5d37f2321c6d9d9c13e66
avinashpitti <avinash87129@gmail.com>
2025-08-17 09:21:54 +0530

    added few more datatype conversions

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 let a="pizza";
 let b="pizza";
 let c="pizza";
+let d="";
+let e="0";
 
 a=Number(a); // here it is considered as nan
 b=String(b); // it is already a string
-c=Boolean(c); // Boolean is considered as true if you give any kind of value except 0.
+c=Boolean(c); // Boolean is considered as true if you give any kind of value except null.
+d=Boolean(d); //here it is false because of null value
+e=Boolean(e); // here it is true because we have given some value.
 
 
 console.log(a,typeof a);
 console.log(b,typeof b);
 console.log(c,typeof c);
+console.log(d,typeof d);
+console.log(e,typeof e);
 
