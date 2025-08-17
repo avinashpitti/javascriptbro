55f886f130408419d0a013218f706290d3bd5eef
avinashpitti <avinash87129@gmail.com>
2025-08-17 10:25:55 +0530

    implement circumference of circle with const pi

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-// change the datatype of a value to another(strings,numbers,booleans)
-let a="pizza";
-let b="pizza";
-let c="pizza";
-let d="";
-let e="0";
+// const
 
-a=Number(a); // here it is considered as nan
-b=String(b); // it is already a string
-c=Boolean(c); // Boolean is considered as true if you give any kind of value except null.
-d=Boolean(d); //here it is false because of null value
-e=Boolean(e); // here it is true because we have given some value.
+const pi=3.14
+let radius;
+let circumference;
 
+radius=window.prompt("Enter the radius of a circle");
+radius=Number(radius);
 
-console.log(a,typeof a);
-console.log(b,typeof b);
-console.log(c,typeof c);
-console.log(d,typeof d);
-console.log(e,typeof e);
+circumference=2*pi*radius;
 
+console.log(circumference);
