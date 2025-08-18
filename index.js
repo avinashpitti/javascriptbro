d148bfed945ba0e6e5ba05434b071374001a98c7
avinashpitti <avinash87129@gmail.com>
2025-08-18 17:19:23 +0530

    added math property

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-//count program
-const decreaseBtn=document.getElementById("decreaseBtn");
-const resetBtn=document.getElementById("resetBtn");
-const increaseBtn=document.getElementById("increaseBtn");
-const countLabel=document.getElementById("countLabel");
+// Math  a built in object that provides a collection of properties and methods.
+document.writeln(Math.PI +"<br>");  
+document.writeln(Math.E+"<br>");
+document.writeln(Math.SQRT2 +"<br>" )
 
-let count=0;
+let x=-3.22;
 
-increaseBtn.onclick=function(){
-    count++;
-    countLabel.textContent=count;
-}
+z=Math.ceil(x);
 
-// here if condition don't let it fall below zero.
-decreaseBtn.onclick=function(){
-    if(count>0){
-    count--;
-    countLabel.textContent=count;}
-}
-
-resetBtn.onclick=function(){
-    count=0;
-    countLabel.textContent=count;
-}
+console.log(z);
