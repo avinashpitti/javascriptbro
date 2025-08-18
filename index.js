4cb2612a32fd20f99f3ba257ad464c4a1d036d7e
avinashpitti <avinash87129@gmail.com>
2025-08-18 09:48:58 +0530

    created a count program

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// const
+//count program
+const decreaseBtn=document.getElementById("decreaseBtn");
+const resetBtn=document.getElementById("resetBtn");
+const increaseBtn=document.getElementById("increaseBtn");
+const countLabel=document.getElementById("countLabel");
 
-const PI=3.14
-let radius;
-let circumference;
+let count=0;
 
-
-document.getElementById("btn").onclick=function(){
-radius=document.getElementById("inp").value ;
-radius=Number(radius);
-circumference=2*PI*radius;
-document.getElementById("hh").textContent=circumference;
+increaseBtn.onclick=function(){
+    count++;
+    countLabel.textContent=count;
 }
 
+// here if condition don't let it fall below zero.
+decreaseBtn.onclick=function(){
+    if(count>0){
+    count--;
+    countLabel.textContent=count;}
+}
 
-console.log(circumference);
+resetBtn.onclick=function(){
+    count=0;
+    countLabel.textContent=count;
+}
