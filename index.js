c3692d8777251226d966f6a9727646804b06e886
avinashpitti <avinash87129@gmail.com>
2025-08-19 23:12:06 +0530

    javascript variables var let const

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const bulb=document.getElementById("myImage");
-const onBtn=document.getElementById("onBtn");
-const offBtn=document.getElementById("offBtn");
+a=3; //never use without var let const
+b=5;
+c=a+b;
 
-const on="bulbon.jpeg";
-const off="bulboff.jpg";
+console.log(c);
 
-onBtn.addEventListener("click", () => {
-  bulb.src = on;
-});
+let d;
+d=33; // You can reassign but can't redeclare
+let e=5;
+let f=d+e;
+console.log(f);
 
-offBtn.addEventListener("click", () => {
-  bulb.src = off;
-});
+var g=3;
+var g=44; // redclare 
+var h;
+var h=6; //reassign
+var i=g+h;
+console.log(i);
+
+const j=3; // you can neither reassign nor redeclare
+const k=5;
+const l=j+k;
+console.log(l);
