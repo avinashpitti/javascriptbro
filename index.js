72e33c6bf6aa8e88da34ff31dcdf44d6b7733b44
avinashpitti <avinash87129@gmail.com>
2025-08-23 21:56:44 +0530

    add javascript errors: try, catch, finally

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-// while loop checks the condition before running the block.
-//Do while loop runs the block first, then checks the condition.
-let i=1;
-do{
-    document.writeln("The number is ",i,"<br>");
-    i++;
-}while(i<=5);
+// Javascript errors:
 
-let x=10;
-do{
-    document.writeln("It runs atlest once even if the condition is wrong ",x);
-    i++;
-}while(x<5);
+// 1. syntax errors: If you make any mistake in syntax like missing the closing parenthesis.
+// 2. Typeerror: example console.lag instead of log
+// 3. Reference error: for ex: x=4; console.log(y); Instead of x you are refering y.
 
+// try: Runs code that might fail
 
+// catch: Catches the error and allows you to handle it
+
+// finally: Runs always, even if there’s an error.
+
+try{
+    const dividend=Number(window.prompt("Enter a dividend: "));
+    const divisor=Number(window.prompt("Enter a divisor: "));
+
+    if(isNaN(dividend) || isNaN(divisor)){
+        throw new Error("Inputs must be numbers");
+    }
+
+    if(divisor===0){
+        throw new Error("can't divide by zero.");
+    }
+
+    const result=dividend/divisor;
+    console.log(result);
+}
+
+catch(error){
+    console.error(error);
+}
+
+finally{
+    console.log("No matter what print this");
+};
